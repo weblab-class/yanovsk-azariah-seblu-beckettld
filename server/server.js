d5fcec477e3931fc9115f59396e2a80318038ec4
yanovsk <yanovsky00@gmail.com>
2023-01-24 11:28:47 -0500

    get problem - 3

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ app.get("/problem", (req, res) => {
   console.log("req params below:");
   console.log(req.query);
   const currentProblem = Problem.find({ version: "mvp" }).then((problem) => {
-    res.send(problem[2].problemText);
+    res.send(problem[currentProblem.length - 3].problemText);
   });
 });
 app.post("/problem", (req, res) => {
