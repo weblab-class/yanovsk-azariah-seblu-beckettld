aeb9cc1525398cf734d5f7d00327173b1514a3fe
yanovsk <yanovsky00@gmail.com>
2023-01-28 21:04:14 -0500

    add sprite selection

--- client/src/components/pages/ChooseMap.js
+++ client/src/components/pages/ChooseMap.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from "react";
 import { SocketContext } from "../context/socket.js";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import Map from "../assets/tile4.gif";
+import { sprites } from "./data.js";
 
 const ChooseMap = () => {
   const socket = useContext(SocketContext);
@@ -24,8 +24,10 @@ const ChooseMap = () => {
     socket.emit("initGameState", { mapSelection, spriteSelection });
   };
   return (
-    <div class="nes-container is-rounded is-dark">
-      <p>Please Choose Map</p>
+    <div className="nes-container is-rounded is-dark">
+      {mapSelection ? "Map " + mapSelection + " chosen" : "Make a map selection"}
+      <br />
+      <br />
 
       <div className="map_tiles_wrapper">
         <Card className="map_tile">
@@ -37,18 +39,83 @@ const ChooseMap = () => {
           <Card.Title class="nes-text is-success">Sahara</Card.Title>
         </Card>
         <Card className="map_tile">
+          <Card.Img variant="top" src={Map} onClick={() => handleMapSelection(3)} />
+          <Card.Title class="nes-text is-success">Harvard</Card.Title>
+        </Card>
+        <Card className="map_tile">
+          <Card.Img variant="top" src={Map} onClick={() => handleMapSelection(1)} />
+          <Card.Title class="nes-text is-warning">Atlantis</Card.Title>
+        </Card>
+      </div>
+      <div className="map_tiles_wrapper">
+        <Card className="map_tile">
+          <Card.Img src={Map} onClick={() => handleMapSelection(1)} />
+          <Card.Title class="nes-text is-warning">Normandy</Card.Title>
+        </Card>
+        <Card className="map_tile">
           <Card.Img variant="top" src={Map} onClick={() => handleMapSelection(2)} />
+          <Card.Title class="nes-text is-error">Kyiv</Card.Title>
+        </Card>
+        <Card className="map_tile">
+          <Card.Img variant="top" src={Map} onClick={() => handleMapSelection(3)} />
+          <Card.Title class="nes-text is-error">Maryland</Card.Title>
+        </Card>
+        <Card className="map_tile">
+          <Card.Img variant="top" src={Map} onClick={() => handleMapSelection(1)} />
+          <Card.Title class="nes-text is-error">MIT</Card.Title>
+        </Card>
+      </div>
+      <br />
+      {spriteSelection ? "Sprite " + spriteSelection + " chosen" : "Make a Sprite Selection"}
+      <br />
+      <br />
+
+      <div className="map_tiles_wrapper">
+        <Card className="sprite_tile">
+          <Card.Img
+            className="sprite_img"
+            src={sprites[4]}
+            onClick={() => handleSpriteSelection(1)}
+          />
+          <Card.Title>Batman</Card.Title>
+        </Card>
+        <Card className="sprite_tile">
+          <Card.Img
+            className="sprite_img"
+            src={sprites[5]}
+            onClick={() => handleSpriteSelection(2)}
+          />
+          <Card.Title>Turtle</Card.Title>
+        </Card>
+        <Card className="sprite_tile">
+          <Card.Img
+            className="sprite_img"
+            src={sprites[6]}
+            onClick={() => handleSpriteSelection(3)}
+          />
+          <Card.Title>Dude</Card.Title>
+        </Card>
+        <Card className="sprite_tile">
+          <Card.Img
+            className="sprite_img"
+            src={sprites[2]}
+            onClick={() => handleSpriteSelection(4)}
+          />
+          <Card.Title>Flesh</Card.Title>
+        </Card>
+        <Card className="sprite_tile">
+          <Card.Img
+            className="sprite_img"
+            src={sprites[8]}
+            onClick={() => handleSpriteSelection(5)}
+          />
+          <Card.Title>Spider</Card.Title>
         </Card>
       </div>
 
-      {mapSelection ? "Map " + mapSelection + " chosen" : ""}
-      <p>Please Choose Sprite</p>
-      <button onClick={() => handleSpriteSelection(1)}>Sprite 1</button>
-      <button onClick={() => handleSpriteSelection(2)}>Sprite 2</button>
-      <button onClick={() => handleSpriteSelection(3)}>Sprite 3</button>
-      {spriteSelection ? "Sprite " + spriteSelection + " chosen" : ""}
-      <p></p>
       <>
+        <br />
+        <br />
         {mapSelection && spriteSelection ? (
           <>
             {submissionMade ? (
